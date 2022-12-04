0c51b57c209dbc191fa92b464bd0cb7250964455
truongnh <truong02.bp@gmail.com>
2022-12-04 11:35:52 +0700

    change host flutter

--- front-end/lib/constants/host_api.dart
+++ front-end/lib/constants/host_api.dart
@@ -1,7 +1,7 @@
-const IP = "localhost";
+const IP = "34.28.66.123";
 const version = "v1";
 final port = 8080;
 final host = "http://$IP:$port/api/$version";
 
-final minioHost = "http://localhost:9000/avatar";
+final minioHost = "http://$IP:9000/avatar";
 
